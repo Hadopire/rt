febf145b5e90ed7eb304f3c13addb2bec749f2e4
Hadopire <hadopire.nc@gmail.com>
2026-04-22 03:11:02 +0200

    typo

--- mini_vg.h
+++ mini_vg.h
@@ -286,7 +286,7 @@ check_nil(nil,p) ? \
 
 typedef char               s8;
 typedef unsigned char      u8;
-typedef char               s16;
+typedef short              s16;
 typedef unsigned short     u16;
 typedef int                s32;
 typedef unsigned int       u32;
